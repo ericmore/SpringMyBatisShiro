8103c4af8ac1ee89733d7bf73c556473e5f119d9
Eric <eric5310@gmail.com>
2018-05-29 17:57:21 +0800

    Init SpringBoot Project integrate with ShiroRedis and MyBatis enhance

---
+++ src/main/java/com/lance/shiro/mapper/TestMapper.java
@@ -0,0 +1,12 @@
+package com.lance.shiro.mapper;
+
+
+import org.apache.ibatis.annotations.Mapper;
+
+/**
+ */
+@Mapper
+public interface TestMapper {
+
+
+}
---
+++ src/main/java/com/lance/shiro/service/ModuleService.java
@@ -0,0 +1,14 @@
+package com.lance.shiro.service;
+
+import java.util.List;
+
+import com.lance.shiro.entity.ModuleInfo;
+
+public interface ModuleService {
+	/**
+	 * 获取角色模块
+	 * @param userId
+	 * @return
+	 */
+	List<ModuleInfo> findModuleByUserId(int userId);
+}
