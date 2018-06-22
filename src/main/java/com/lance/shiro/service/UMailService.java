af11518cbb879bda102e771fa8e2c9c53ea57ae1
Zhang Yunfeng <578210452@qq.com>
2018-06-22 13:33:14 +0800

    umail api test

---
+++ src/main/java/com/lance/shiro/service/UMailService.java
@@ -0,0 +1,216 @@
+package com.lance.shiro.service;
+
+import com.lance.shiro.utils.ConvertUtils;
+import org.apache.commons.lang.StringUtils;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.dom4j.DocumentException;
+import org.dom4j.DocumentHelper;
+import org.dom4j.Element;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+
+import org.dom4j.Document;
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+
+@Service
+public class UMailService{
+    private static Logger logger = LogManager.getLogger();
+
+    @Value("${mail.domain}")
+    private String mailDomain;
+
+    @Value("${mail.server}")
+    private String mailServer;
+
+    @Value("${mail.api}")
+    private String mailApi;
+
+    @Value("${mail.admin.username}")
+    private String mailAdmin;
+
+    @Value("${mail.admin.password}")
+    private String adminPassword;
+
+    @Value("${mail.manager.username}")
+    private String mailManager;
+
+    @Value("${mail.manager.password}")
+    private String managerPassword;
+
+    @Value("${mail.manager.nickname}")
+    private String managerNick;
+
+    @Value("${mail.defaultPassword}")
+    private String defaultPassword;
+
+    @Autowired
+    private RestTemplate restTemplate;
+
+    public List searchMailBox(String mailAccount) {
+        String url = "http://" + mailServer + "/webmail/api.php?do=getMailbox";
+
+        return null;
+    }
+
+    /**
+     * <pre>
+     * username：	域管理员的账号（非空）
+     password：	域管理员的密码，MD5加密（非空）
+     domain:		域管理员所在域名，默认值是主域
+     account：		添加该域名下的邮箱账号，如abc（非空）
+     pwd：		该邮箱账号的密码，明文（非空）
+     realname：	该邮箱账号的真实姓名，如abc（非空）
+     mbsize：		该邮箱账号的邮件空间大小，数字型，单位M，如100
+     ndsize：		该邮箱账号的网络硬盘大小，数字型，单位M，如100
+     disabled：	该邮箱账号是否被禁用，选项型，1是禁用，-1是非禁用
+     limit_send：	该邮箱发信功能是否被限制，选项型，1是限制，-1是正常
+     limit_recv:	该邮箱收信功能是否被限制，选项型，1是限制，-1是正常
+     recvsms：		该邮箱短信通知是否被限制，选项型，1是限制，-1是正常
+     limit_ip：		该邮箱的限制登录的IP（段）地址
+     eenumber：	该邮箱用户的工号
+     tel_mobile：	该邮箱用户的手机号码
+     tel_home：	该邮箱用户的住宅电话
+     tel_qq：		该邮箱用户的qq
+     tel_msn：		该邮箱用户的msn
+     tel_work：	该邮箱用户的公司电话
+
+     adminpass： 超级管理员密码MD5，如cd2c3a3529b259d42718a6a606c63f0a（password不知道为空时非空）
+     adminname：	超级管理员用户名如：admin（password不知道为空时非空）
+
+     * </pre>
+     *
+     */
+    public Map<String, String> addMailBox(String username, String realname, String password, Map<String,String> otherParams) {
+        String url =mailApi+"?do=addMailbox";
+
+        Map<String, String> ret = new HashMap<String, String>();
+
+
+        if (StringUtils.isBlank(username)) {
+            ret.put("status", "-1");
+            ret.put("data", "account  cant be null");
+            return ret;
+        }
+
+
+        if (StringUtils.isBlank(realname)) {
+            ret.put("status", "-1");
+            ret.put("data", "realname cant be null");
+            return ret;
+        }
+        if (StringUtils.isBlank(password)) {
+            password = defaultPassword;
+        }
+        MultiValueMap<String, String> map = new LinkedMultiValueMap<>();
+        map.add("account", username);
+        map.add("pwd", password);
+        map.add("username", mailAdmin);
+        map.add("password", adminPassword);
+        map.add("domain", mailDomain);
+        map.add("realname", realname);
+        String responseText = restTemplate.postForObject(url, map, String.class);
+        logger.debug(responseText);
+        ret = parseSimpleResult(responseText);
+        ret.put("mail",username+"@"+mailDomain);
+
+        return ret;
+    }
+
+    /**
+     * 删除
+     * @param username
+     * @return
+     */
+    public Map<String, String> delMailbox(String username) {
+        String url =mailApi+"?do=delMailbox";
+
+        Map<String, String> ret = new HashMap<String, String>();
+
+
+        if (StringUtils.isBlank(username)) {
+            ret.put("status", "-1");
+            ret.put("data", "account  cant be null");
+            return ret;
+        }
+
+        MultiValueMap<String, String> map = new LinkedMultiValueMap<>();
+        map.add("account", username);
+        map.add("username", mailAdmin);
+        map.add("password", adminPassword);
+        map.add("domain", mailDomain);
+        String responseText = restTemplate.postForObject(url, map, String.class);
+        logger.debug(responseText);
+        ret = parseSimpleResult(responseText);
+        return ret;
+    }
+
+    /**
+     * 通过管理员帐号对外发送邮件
+     * @param subject
+     * @param body
+     */
+    public Map<String,String> sendManagerMail(String to,String subject,String body) {
+        Map<String, String> ret = new HashMap<String, String>();
+        if(StringUtils.isBlank(to))
+        {
+            ret.put("status", "-1");
+            ret.put("data", "receiver cant be empty");
+            return ret;
+        }
+
+        if(StringUtils.isBlank(subject))
+        {
+            subject="no subject";
+        }
+        MultiValueMap<String, String> map = new LinkedMultiValueMap<>();
+        map.add("email", mailManager);
+        map.add("password", managerPassword);
+        map.add("to", to);
+        map.add("subject", subject);
+
+        body+="<div><br><br><br></div>";
+        map.add("body", body);
+        String url=mailApi+"?do=sendMail";
+        System.out.println(map);
+        System.out.println(url);
+        String responseText = restTemplate.postForObject(url, map, String.class);
+        logger.debug(responseText);
+        ret = parseSimpleResult(responseText);
+        return ret;
+    }
+
+    public void getMailbox(String keyword)
+    {
+        Map<String,String> map=new HashMap<String,String>();
+    }
+
+    public void deleteMailbox(String mailAccount) {
+
+    }
+
+    private Map<String, String> parseSimpleResult(String text) {
+        Document doc = null;
+        Map<String, String> ret = new HashMap<String, String>();
+        try {
+            doc = DocumentHelper.parseText(text);
+            ret = (Map<String, String>) ConvertUtils.xml2map(doc.getRootElement());
+        } catch (DocumentException e) {
+            // TODO Auto-generated catch block 
+            log.error(e);
+            e.printStackTrace();
+            return null;
+        }
+        return ret;
+
+    }
+}
--- src/main/java/com/lance/shiro/web/MailController.java
+++ src/main/java/com/lance/shiro/web/MailController.java
@@ -1,5 +1,7 @@
 package com.lance.shiro.web;
 
+import com.lance.shiro.service.UMailService;
+import com.lance.shiro.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
@@ -8,6 +10,9 @@ import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.Map;
 
 
 /**
@@ -18,22 +23,85 @@ import org.springframework.web.bind.annotation.RestController;
 public class MailController extends BaseController {
 
     @Autowired
-    private JavaMailSender mailSender; //自动注入的Bean
-
-    @Value("${spring.mail.username}")
-    private String Sender; //读取配置文件中的参数
+    private UMailService uMailService;
 
     @RequestMapping(value = "", method = RequestMethod.GET)
     public ResponseEntity mail() {
-        SimpleMailMessage message = new SimpleMailMessage();
-        message.setFrom(Sender);
-        message.setTo("578210452@qq.com");
-        message.setSubject("主题：简单邮件");
-        message.setText("测试邮件内容");
-        mailSender.send(message);
-
+        Map<String,String> otherParams=new HashMap<String,String>();
+//        uMailService.addMailBox("test123","test123","iPanPassw0rd",otherParams);
+        uMailService.delMailbox("test123");
+//        uMailService.sendManagerMail("578210452@qq.com","test1","test123");
         return success("ok");
     }
 
+//    /**
+//     * chuangjian系统邮件
+//     * @param u
+//     * @return
+//     */
+//    public static boolean createSystemMail(User u)
+//    {
+//
+//        String name=u.getName();
+//
+//        String code=u.getCode();
+//
+//        Map<String,String> otherParams=new HashMap<String,String>();
+//
+//        Map<String,String> ret=UMailServerAPI.addMailBox(code, name, "", otherParams);
+//        if(ret==null)
+//        {
+//            System.out.println("createSystemMail error,return null");
+//        }
+//        if(ret.containsKey("status"))
+//        {
+//            String status=ret.get("status");
+//            String data=ret.get("data");
+//            if(status.equalsIgnoreCase("0"))
+//            {
+//                System.out.println("have createSystemMail success:"+data);
+//                String mail=ret.get("mail");
+//                u.setSystemMail(mail);
+//                u.setUpdateDate(new Date());
+//                userService.updateSystemMail(u);
+//
+//            }
+//        }
+//        return true;
+//
+//    }
+
+//
+//    public static void notifyEmail(UserAgent ra) {
+//        String defaultPassword = GlobalPropertyUtil.getConfig("mail.defaultPassword");
+//        String subject = "Dear " + ra.getFirstname() + " " + ra.getSurname() + ", Welcome To the iPAN!";
+//        String body = "<div>Welcome to the international Property Agent network(iPAN)</div>";
+//        // body+="<div>Your member ID:"+u.getCode()+",system mail is
+//        // "+u.getSystemMail()+"(default password is :"+defaultPassword+")</div>";
+//        body += "<div>Your member ID:" + ra.getCode() + "</div>";
+//        body += "<div>Please confirm your detail below</div>";
+//        body += "<div>Mobile Number:" + (ra.getMobile()==null?"none":ra.getMobile()) + "</div>";
+//        body += "<div>Country/Region:" + ra.getCountry() + "</div>";
+//        body += "<div>State/Province:" + (ra.getState()==null?"none":ra.getState()) + "</div>";
+//        body += "<div>City:" + (ra.getCity()==null?"none":ra.getCity()) + "</div>";
+//        body += "<div>Street:" + (ra.getStreet()==null?"none":ra.getStreet()) + "</div>";
+//        body += "<div>Occupation:" + (ra.getOccupation()==null?"none":ra.getOccupation() ) + "</div>";
+//        body += "<div>Please contact info@ipan.net.au if detail need to be corrected.</div>";
+//        body += "<div>Now you may use your Member ID to RSVP the coming up Event.</div>";
+//        body += "<div><br><br>    iPAN Admin Team </div>";
+//        String to = ra.getEmail();
+//
+//        Map<String, String> ret = UMailServerAPI.sendManagerMail(to, subject, body);
+//        if (ret.containsKey("status")) {
+//            String status = ret.get("status");
+//            String data = ret.get("data");
+//            if (status.equalsIgnoreCase("0")) {
+//                System.out.println("have notify user success:" + data);
+//                ra.setStatus(9);
+//                regAgentService.updateStatus(ra);
+//            }
+//
+//        }
+//    }
 
 }
