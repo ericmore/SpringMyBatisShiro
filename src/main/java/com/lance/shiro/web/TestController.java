21c06a83ca4e93f98d04256f23fc356c2f45492e
Eric <eric5310@gmail.com>
2018-07-07 21:33:34 +0800

    增加测试接口

---
+++ src/main/java/com/lance/shiro/web/TestController.java
@@ -0,0 +1,34 @@
+package com.lance.shiro.web;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@RestController
+@RequestMapping("/rest/test/")
+public class TestController {
+
+    @Value("${spring.datasource.url}")
+    private String url;
+
+    @Value("${rootFilePath}")
+    private String rootFilePath;
+
+    @Value("${rootHttpPath}")
+    private String rootHttpPath;
+
+    @RequestMapping(value = "env", method = RequestMethod.GET)
+    public ResponseEntity getEnv(){
+        Map<String, Object> env = new HashMap<>();
+        env.put("spring.datasource.url", url);
+        env.put("rootFilePath", rootFilePath);
+        env.put("rootHttpPath", rootHttpPath);
+        return ResponseEntity.ok(env);
+    }
+
+}
